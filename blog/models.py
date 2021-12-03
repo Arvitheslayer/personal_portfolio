37aed1d10bd71635a96f46b9a01a899b95cc976a
Arvitheslayer <89351600+Arvitheslayer@users.noreply.github.com>
2021-12-03 22:02:22 +0200

    css added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class All_blogs(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    description = models.CharField(max_length=250)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import All_blogs
 
-# Create your views here.
+def all_blogs(request):
+    blogs = All_blogs.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs} )
