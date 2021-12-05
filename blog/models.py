fa3ad3c93f83f1ef0476381d9b433f892b77deec
Arvitheslayer <89351600+Arvitheslayer@users.noreply.github.com>
2021-12-05 17:38:01 +0200

    some changes added,mechanics are ready

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class All_blogs(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     date = models.DateTimeField()
-    description = models.CharField(max_length=250)
+    description = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import All_blogs
 
 def all_blogs(request):
-    blogs = All_blogs.objects.all()
+    blogs = All_blogs.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs':blogs} )
+
+def detail(request, blog_id):
+    blog = get_object_or_404(All_blogs, pk=blog_id)
+    return render(request, 'blog/detail.html', { 'blog': blog})
